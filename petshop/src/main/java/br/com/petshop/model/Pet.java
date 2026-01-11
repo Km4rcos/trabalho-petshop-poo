1cd2d5046df4a181c2cda16381752cff70ff2055
Brenda <brendamottac@gmail.com>
2026-01-11 00:31:45 -0300

    Release 1.3

--- petshop/src/main/java/br/com/petshop/model/Pet.java
+++ petshop/src/main/java/br/com/petshop/model/Pet.java
@@ -1,10 +1,25 @@
 package br.com.petshop.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pet extends Animal {
     private Cliente dono;
 
-    public Pet() {super();}
+    private List<Servico> historicoServicos = new ArrayList<>();
+
+    public Pet() {
+        super();
+    }
 
     public Cliente getDono() { return dono; }
     public void setDono(Cliente dono) { this.dono = dono; }
+
+    public List<Servico> getHistorico() {
+        return this.historicoServicos;
+    }
+
+    public void adicionarServico(Servico servico) {
+        this.historicoServicos.add(servico);
+    }
 }
