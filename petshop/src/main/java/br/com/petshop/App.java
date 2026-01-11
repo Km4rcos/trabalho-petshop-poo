9162b5c7cff17e4a0eecdd75a272b944373c04b1
Brenda <brendamottac@gmail.com>
2026-01-11 15:54:28 -0300

    Adiciona teste de integração entre Cliente e Pet

--- petshop/src/main/java/br/com/petshop/App.java
+++ petshop/src/main/java/br/com/petshop/App.java
@@ -1,33 +1,36 @@
 package br.com.petshop;
 
-import java.util.List;
-
 import br.com.petshop.dao.ClienteDAO;
+import br.com.petshop.dao.PetDAO;
 import br.com.petshop.model.Cliente;
+import br.com.petshop.model.Pet;
 
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Sistema PetShop - Relatório de Clientes");
+        System.out.println("🚀 Testando Integração Cliente + Pet");
 
         try {
-            ClienteDAO dao = new ClienteDAO();
+            
+            ClienteDAO clienteDao = new ClienteDAO();
+            PetDAO petDao = new PetDAO();
 
-            List<Cliente> lista = dao.listarTodos();
-
-            System.out.println("-----------------------------------------");
-            if (lista.isEmpty()) {
-                System.out.println("Nenhum cliente cadastrado ainda.");
-            } else {
-                for (Cliente c : lista) {
-                    System.out.println("Nome: " + c.getNome());
-                    System.out.println("Email: " + c.getEmail());
-                    System.out.println("-----------------------------------------");
-                }
+            String cpfMarcos = "999.888.777-66";
+            Cliente marcos = new Cliente(cpfMarcos, "Marcos Silva", "marcos@email.com", "11 90000-0000");
+            
+            try {
+                clienteDao.salvar(marcos);
+            } catch (Exception e) {
+                System.out.println("⚠️ Aviso: Talvez o Marcos já exista no banco.");
             }
 
+            Pet rex = new Pet("Rex", "Cachorro", cpfMarcos); 
+            petDao.salvar(rex);
+
+            System.out.println("Teste finalizado!");
+
         } catch (Exception e) {
-            System.out.println("Erro:");
+            System.out.println("Erro fatal:");
             e.printStackTrace();
         }
     }
