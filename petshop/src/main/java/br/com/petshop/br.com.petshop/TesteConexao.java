005412dabd0af360acd36359b5eaaeb70ba5024c
Brenda <brendamottac@gmail.com>
2026-01-11 03:59:25 -0300

    Adiciona teste de integração para conexão com banco de dados

---
+++ petshop/src/main/java/br/com/petshop/br.com.petshop/TesteConexao.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import br.com.petshop.dao.ConnectionFactory; 
+
+public class TesteConexao {
+    
+    public static void main(String[] args) {
+        System.out.println("--- INICIANDO TESTE ---");
+
+        try {
+            
+            Connection con = ConnectionFactory.getConnection();
+            
+            if (con != null) {
+                System.out.println("✅ SUCESSO! O Banco de Dados conectou!");
+                con.close();
+            }
+            
+        } catch (Exception e) {
+            System.out.println("❌ ERRO: Ocorreu um problema.");
+            e.printStackTrace();
+        }
+    }
+}
