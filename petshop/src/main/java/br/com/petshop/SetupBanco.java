e4b2672e2367c8e6c47fd7dcaa1e75a9568d9643
Brenda <brendamottac@gmail.com>
2026-01-11 15:54:05 -0300

    Adiciona criação da tabela pets com chave estrangeira

--- petshop/src/main/java/br/com/petshop/SetupBanco.java
+++ petshop/src/main/java/br/com/petshop/SetupBanco.java
@@ -2,7 +2,6 @@ package br.com.petshop;
 
 import java.sql.Connection;
 import java.sql.Statement;
-
 import br.com.petshop.dao.ConnectionFactory;
 
 public class SetupBanco {
@@ -10,24 +9,30 @@ public class SetupBanco {
     public static void main(String[] args) {
         try {
             System.out.println("🔨 Criando tabelas no banco...");
-            
             Connection con = ConnectionFactory.getConnection();
             Statement st = con.createStatement();
 
-            String sql = "CREATE TABLE IF NOT EXISTS clientes ("
+            String sqlCliente = "CREATE TABLE IF NOT EXISTS clientes ("
                     + "cpf TEXT PRIMARY KEY, "
                     + "nome TEXT, "
                     + "email TEXT, "
                     + "telefone TEXT"
                     + ")";
+            st.execute(sqlCliente);
+
+            String sqlPet = "CREATE TABLE IF NOT EXISTS pets ("
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "nome TEXT, "
+                    + "tipo TEXT, " 
+                    + "cpf_dono TEXT, " 
+                    + "FOREIGN KEY(cpf_dono) REFERENCES clientes(cpf)"
+                    + ")";
+            st.execute(sqlPet);
             
-            st.execute(sql);
-            
-            System.out.println("Tabela 'clientes' criada com sucesso!");
+            System.out.println("Tabelas 'clientes' e 'pets' criadas/verificadas!");
             con.close();
             
         } catch (Exception e) {
-            System.out.println("Erro ao criar tabelas:");
             e.printStackTrace();
         }
     }
